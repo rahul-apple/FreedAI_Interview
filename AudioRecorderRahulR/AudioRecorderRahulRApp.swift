6edb16313dd79e3c13ccefb84de09fc066f51888
Rahul R <rahulthazhuthala@gmail.com>
2024-06-28 15:41:12 +0530

    Initial Commit

---
+++ AudioRecorderRahulR/AudioRecorderRahulRApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderRahulRApp.swift
+//  AudioRecorderRahulR
+//
+//  Created by Rahul R on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderRahulRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
